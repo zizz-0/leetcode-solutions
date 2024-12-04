eeae054f9d058408b2bd910798e6a1ff4d52807a
Zoe Rizzo <rizzo.zoej@gmail.com>
2024-12-04 15:57:20 -0500

    import

--- easy/27_remove_element.py
+++ easy/27_remove_element.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
